8dcfb2fb6c39086509f849c60ac7f4ebb3716358
LuisHernandez1389 <luilmhg70@gmail.com>
2023-04-02 13:19:28 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,102 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
 
 function App() {
+  const [elements, setElements] = useState([]);
+  const [editingElement, setEditingElement] = useState(null);
+  useEffect(() => {
+    fetch('https://api-generator.retool.com/nxXN2b/jsonserver')
+      .then(response => response.json())
+      .then(data => setElements(data))
+      .catch(error => console.error(error));
+  }, []);
+
+  const addElement = (element) => {
+    fetch('https://api-generator.retool.com/nxXN2b/jsonserver', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(element)
+    })
+      .then(response => response.json())
+      .then(data => setElements([...elements, data]))
+      .catch(error => console.error(error));
+  };
+
+  const updateElement = (id, updatedElement) => {
+    fetch(`https://api-generator.retool.com/nxXN2b/jsonserver/${id}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(updatedElement)
+    })
+      .then(response => response.json())
+      .then(data => {
+        const index = elements.findIndex(element => element.id === id);
+        const updatedElements = [...elements];
+        updatedElements[index] = data;
+        setElements(updatedElements);
+      })
+      .catch(error => console.error(error));
+  };
+
+  const deleteElement = (id) => {
+    fetch(`https://api-generator.retool.com/nxXN2b/jsonserver/${id}`, {
+      method: 'DELETE'
+    })
+      .then(() => setElements(elements.filter(element => element.id !== id)))
+      .catch(error => console.error(error));
+  };
+  const editElement = (element) => {
+    setEditingElement(element);
+  };
+
+  const cancelEdit = () => {
+    setEditingElement(null);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Elements</h1>
+      <ul>
+        {elements.map(element => (
+          <li key={element.id}>
+            {editingElement && editingElement.id === element.id ? (
+              <form onSubmit={(event) => {
+                event.preventDefault();
+                const name = event.target.elements.name.value;
+                const lastname = event.target.elements.lastname.value;
+                updateElement(element.id, { name, lastname });
+              }}>
+                <input type="text" name="name" defaultValue={element.name} />
+                <input type="text" name="lastname" defaultValue={element.lastname} />
+                <button type="submit">Save</button>
+                <button type="button" onClick={() => cancelEdit()}>Cancel</button>
+              </form>
+            ) : (
+              <>
+                <span>{element.name}  {element.lastname}  </span>
+               
+                <button onClick={() => deleteElement(element.id)}>Delete   </button>
+                <button onClick={() => editElement(element)}>   Edit</button>
+              </>
+            )}
+          </li>
+        ))}
+      </ul>
+      {!editingElement && (
+        <form onSubmit={(event) => {
+          event.preventDefault();
+          const name = event.target.elements.name.value;
+          const lastname = event.target.elements.lastname.value;
+          addElement({ name, lastname });
+          event.target.reset();
+        }}>
+          <input type="text" name="name" placeholder="Name" />
+          <input type="text" name="lastname" placeholder="lastname" />
+          <button type="submit">Add</button>
+        </form>
+      )}
     </div>
   );
 }
